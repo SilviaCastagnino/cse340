db70f5e50093fc49a1d1ab32a7c0878d73d00c05
SilviaCastagnino <sylvia19976@gmail.com>
2025-09-27 12:50:10 +0200

    New changes

---
+++ routes/accountRoute.js
@@ -0,0 +1,21 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router()
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
+// Route to build path to my account
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+// Route to build register path
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -13,6 +15,8 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
+const account = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * View Engine and Templates
@@ -20,6 +24,30 @@ const utilities = require("./utilities/")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * Routes
@@ -29,6 +57,8 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// Account routes
+app.use("/account", account)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
